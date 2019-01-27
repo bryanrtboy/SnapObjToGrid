d3d97e79f8c5ca5779701d6e84958d174a107838
bryanrtboy <bryan@bryanleister.com>
2019-01-26 21:23:50 -0700

    Initial commit

---
+++ Assets/SnapToGrid/Scripts/AddObjectToScene.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddObjectToScene : MonoBehaviour
+{
+    public static AddObjectToScene instance;
+    public Camera m_mainCamera;
+    public GameObject m_confirmButton;
+
+    [HideInInspector]
+    public bool m_isAttached = false;
+    [HideInInspector]
+    public GameObject m_newObject;
+
+    void Awake()
+    {
+        if (instance == null)
+            //if not, set instance to this
+            instance = this;
+        //If instance already exists and it's not this:
+        else if (instance != this)
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        if (m_confirmButton != null)
+            m_confirmButton.SetActive(false);
+    }
+    void Update()
+    {
+        if (m_isAttached)
+        {
+            Vector3 pos = m_mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -m_mainCamera.transform.position.z));
+            m_newObject.transform.position = pos;
+
+            bool canRelease = false;
+            RaycastHit hit;
+            Ray ray = m_mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Debug.Log("Ray hit " + hit.transform.name + " at " + Time.time);
+                if (hit.collider.tag == "Sketch")
+                {
+                    canRelease = true;
+                }
+            }
+
+            if (Input.GetMouseButtonDown(0) && canRelease)
+            {
+                pos = m_mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -m_mainCamera.transform.position.z));
+                m_newObject.transform.position = pos;
+                m_isAttached = false;
+                m_newObject = null;
+            }
+        }
+
+    }
+
+    public void AddObject(GameObject g)
+    {
+        if (m_isAttached)
+            return;
+
+        if (m_mainCamera)
+        {
+            Vector3 pos = m_mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, m_mainCamera.transform.position.z));
+            m_newObject = Instantiate(g, pos, Quaternion.identity);
+            m_isAttached = true;
+        }
+    }
+
+    void ChangeLayer(GameObject g, int layerID)
+    {
+        Transform[] children = g.GetComponentsInChildren<Transform>();
+
+        foreach (Transform t in children)
+        {
+            t.gameObject.layer = layerID;
+        }
+        g.layer = layerID;
+
+    }
+
+    void UpdateColliders(GameObject g, bool isColliderActive)
+    {
+        Collider[] colliders = g.GetComponentsInChildren<Collider>();
+        foreach (Collider mc in colliders)
+        {
+            mc.enabled = isColliderActive;
+        }
+    }
+}
---
+++ Assets/SnapToGrid/Scripts/SnapToCollider.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToCollider : MonoBehaviour
+{
+    public float m_yOffset = .5f;
+    Color m_paintColor = Color.white;
+    Texture m_texture;
+
+    bool m_isAttached = false;
+    MeshRenderer m_mr;
+
+    void Start()
+    {
+        if (AddObjectToScene.instance == null)
+        {
+            Debug.LogError("No cursor tex available!");
+            Destroy(this);
+        }
+        m_mr = this.GetComponent<MeshRenderer>();
+        m_paintColor = m_mr.material.color;
+    }
+    void OnMouseEnter()
+    {
+        AddObjectToScene.instance.m_isAttached = false;
+        m_mr.material.color = Color.red;
+        if (AddObjectToScene.instance.m_newObject != null)
+        {
+            AddObjectToScene.instance.m_newObject.transform.position = new Vector3(transform.position.x, transform.position.y + m_yOffset, transform.position.z);
+        }
+    }
+
+    void OnMouseExit()
+    {
+        //AddObjectToScene.instance.m_isAttached = true;
+        m_mr.material.color = m_paintColor;
+    }
+
+    void OnMouseDown()
+    {
+        //If no object is currently in play, do nothing. 
+        if (AddObjectToScene.instance.m_newObject == null)
+            return;
+
+
+        //Otherwise, as long as we have a confirm GUI, let's make it active
+        if (AddObjectToScene.instance.m_confirmButton != null)
+        {
+            Vector3 pos = new Vector3(transform.position.x + 1, transform.position.y + m_yOffset, transform.position.z);
+            AddObjectToScene.instance.m_confirmButton.transform.position = pos;
+            AddObjectToScene.instance.m_confirmButton.SetActive(true);
+        }
+
+        AddObjectToScene.instance.m_newObject = null;
+        this.GetComponent<Collider>().enabled = false;
+    }
+}
